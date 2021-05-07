c5fd69d510d90c7b07a508db113246b124344102
InRabeGH <rabe_edgar@hotmail.com>
2021-05-07 14:15:59 -0500

    Se agrego boton de posicion hasta arriba, asi como se adapto el menu para pantallas medianas, se agregaron funciones JS con Querty, Se actualizo bootstrap y JQuery a las versiones mas actuales, se modificaron los archivos para la adaptacion

---
+++ docs/assets/JS/boton-flotante.js
@@ -0,0 +1,20 @@
+// Variables
+var amountScrolled = 200;
+var amountScrolledNav = 25;
+
+// Agregar clase a un boton dependiendo de una accion
+$(window).scroll(function() {
+if ( $(window).scrollTop() > amountScrolled ) {
+    $('button.boton-flotante').addClass('show');
+} else {
+    $('button.boton-flotante').removeClass('show');
+}
+});
+
+// Al accionar un boton se ejecuta una animacion, en este caso el desplazamiento de la posicion actual a 0 con el scroll
+$('button.boton-flotante').click(function() {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 800);
+    return false;
+});

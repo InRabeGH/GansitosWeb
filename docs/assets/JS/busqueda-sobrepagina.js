33e6680af49467d0fed0f057f532199c3104427b
InRabeGH <rabe_edgar@hotmail.com>
2021-05-14 20:45:50 -0500

    Se agrego el boton busqueda para todas las resoluciones y su fucionalidad

--- docs/assets/JS/busqueda-sobrepagina.js
+++ docs/assets/JS/busqueda-sobrepagina.js
@@ -1,7 +1,29 @@
+// Funciones para ver o ocultar el cuadro de busqueda en pantalla completa
 function openSearch() {
-    document.getElementById("myOverlay").style.display = "block";
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+function closeSearch() {
+  document.getElementById("myOverlay").style.display = "none";
+}
+
+// Funcion filtro para poder seleccionar lo que buscamos mientras lo escribimos
+function filterFunction() {
+  var input, filter, a, i, txtValue;
+
+  input = document.getElementById("buscar");        // Se guarda en una variable la infromacion que se va escribiendo en el input
+  filter = input.value.toUpperCase();               // Lo que se va escribiendo se guarda en un formato especifico
+  div = document.getElementById("overlayContent");  // Se guarda el contenedor de todas las etiquetas "a" de opciones
+  a = div.getElementsByTagName("a");                // Se guardan todas las etiquetas donde vienen las opciones
+
+  // Ciclo para ir comparando letra por letra y mostrando opciones
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
   }
-  
-  function closeSearch() {
-    document.getElementById("myOverlay").style.display = "none";
-  }
+}
+
